7f2aca0d1e2957578256361cd34c6a2092528c63
PedroSincero <dckpedro@gmail.com>
2022-01-19 17:16:46 -0300

    Finalizando de Fato o Requisito 2 - [DONE]

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    elif len(word) <= 2:
+        return True
+    else:
+        return is_palindrome_recursive(word[1: -1], 0, -1)
