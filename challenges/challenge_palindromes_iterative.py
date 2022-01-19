b59441e2803af4ed10b245cb432e5bdecb4cd612
PedroSincero <dckpedro@gmail.com>
2022-01-19 15:49:44 -0300

    Fix - FInalizando Requisito 2 - [DONE]

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,19 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    elif word[0] != word[-1]:
+        return False
+    elif len(word) <= 2:
+         return True
+    else:
+        result = word[1:-1]
+        return is_palindrome_iterative(result)
+
+    # return True
+    # return False
+
+# se a letra inicial for igual a final
+# vá para outra casa
+#  se a string acabar, retorne True
+#  se a letra inicial nao for igual a final, retorne false
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,4 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+
+# Qual horario tem maior qtd de pessoas
