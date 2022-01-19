708852915a6e11f847874cd245ba489067910db9
PedroSincero <dckpedro@gmail.com>
2022-01-19 18:12:47 -0300

    fix - finalizando requisito 1 - [DONE]

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    # https://pt.stackoverflow.com/questions/176525/como-posso-saber-se-a-vari%C3%A1vel-%C3%A9-um-n%C3%BAmero-inteiro-em-python
+    if not isinstance(target_time, int):
+        return None
+    for p in permanence_period:
+        if not isinstance(p[0], int) or not isinstance(p[1], int):
+            return None
+        if p[0] <= target_time and p[1] >= target_time:
+            count += 1
+    return count
